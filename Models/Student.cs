c04a5ff709445ca693b693ad02b797cef3b43322
Ѓорѓиевски Филип <filip.gjorgjievski@students.finki.ukim.mk>
2021-12-12 02:50:35 +0100

    12/12/2021 2:50

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }    
+        public ICollection<Enrollment> Entrollments { get; set; }
+
+
+    }
+}
