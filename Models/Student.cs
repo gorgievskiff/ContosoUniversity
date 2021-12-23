817ec517419ae80b458e1182453e2ca3c09228d7
Ѓорѓиевски Филип <filip.gjorgjievski@students.finki.ukim.mk>
2021-12-23 15:40:45 +0100

    Create a complex data model (Customize data model)

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,24 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int StudentId { get; set; }
+
+
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         public string LastName { get; set; }
+
+
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }    
+
+
+        [DataType(DataType.Date)] // Don't provide any validation, just for appearance
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; } 
+        
+
         public ICollection<Enrollment> Enrollments { get; set; }
 
 
